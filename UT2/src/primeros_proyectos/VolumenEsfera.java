a5d221aee47c0549fb65140eda0b83ae7832832d
mgargod <magarciagodoy@yahoo.es>
2013-10-18 17:07:54 +0200

    Actualización 18/10/2013

--- UT2/src/primeros_proyectos/VolumenEsfera.java
+++ UT2/src/primeros_proyectos/VolumenEsfera.java
@@ -8,6 +8,8 @@ package primeros_proyectos;
 //ASIGNATURA: Programación
 //CURSO: Primero DAW
 
+//18/10/2013-ACTUALIZACIÓN: Utilizamos la clase Math para la constante PI.
+
 import java.util.Scanner; //importamos la clase Scanner para usarla en la aplicación
 
 public class VolumenEsfera {
@@ -17,7 +19,7 @@ public class VolumenEsfera {
 		Scanner teclado = new Scanner( System.in ); //creamos el objeto teclado para introducción de datos
 		
 		int radio; //declaración de variable entera;
-		double PI = 3.1415927; //declaración y asignación de valor de constante PI
+		final double PI = Math.PI; //declaración y asignación de valor de constante PI
 		double volumen; //declaración de variable real
 							
 		System.out.print( "Introduzca el RADIO de la esfera (número entero positivo): ");	//petición de radio
@@ -27,7 +29,7 @@ public class VolumenEsfera {
 		teclado.close(); //cerramos introducción de datos por teclado
 		
 		volumen = 4 * PI * radio * radio * radio / 3; //asignamos resultado de la operación a volumen
-				
+		//Podríamos haber utilizado el Math.pow(radio, 3) para elevar el radio al cubo		
 		System.out.println( "------RESULTADOS------"); //imprime cabecera "Resultados"
 		
 		System.out.println( "Volumen: " + volumen ); //imprime volumen
