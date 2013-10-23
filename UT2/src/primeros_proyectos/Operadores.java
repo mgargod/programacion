938b961c5958631b2b574b3042d28c56b8b102ef
mgargod <magarciagodoy@yahoo.es>
2013-10-23 20:37:04 +0200

    Subido ejercicio de precedencias de operadores en el package "primeros_proyectos" dentro de la carpeta "UT2".

---
+++ UT2/src/primeros_proyectos/Operadores.java
@@ -0,0 +1,31 @@
+package primeros_proyectos;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+
+		System.out.println(((-5)*(-5) + 10)*3);//105
+		System.out.println(35.3 - 53 / 10);//30.299999999999997
+		System.out.println(1 + 5 % 1.5 * 8 / 2);//3.0
+		System.out.println(-(-Math.pow(2, 5) + 18 / 5));//29.0
+		System.out.println(65 / 3 / 4.0 * 3);//15.75
+		System.out.println(44 % 10 + 7 - Math.pow(25, 10) - 2);//-9.5367431640616E13
+		System.out.println(4.0 / 10.0 + 3.5 * 2);//7.4
+		System.out.println(10 % 4 + 6 / 2);//5
+		System.out.println(Math.pow(Math.abs(4-20/3), 3));//8.0
+		System.out.println(Math.sqrt(4.5-5.0) + 7*3);//NaN
+		System.out.println(3*10/3 + 10%3);//11
+		System.out.println(Math.pow(3L, 3));//27.0
+		System.out.println((36.0/4)+(5*3));//24.0
+		System.out.println((28/7)*2);//8
+		System.out.println((13*3)%10);//9
+		System.out.println(Math.pow(Math.pow(2,3), 3));//512.0
+		
+		
+		
+		
+		
+
+	}
+
+}
